a36c798921d1ff69f56885fe2584aac979a99bed
suchov <artem.suchov@gmail.com>
2019-04-06 14:21:54 +0300

    add everything withot gitignore

---
+++ server.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === database.users[0].email && 
+      req.body.password === database.users[0].password){
+    res.json('success')
+  }else{
+    res.status(400).json('error logging in')
+  }
+})
+
+app.post('/register', (req, res) => {
+  const {email, name, password} = req.body;
+  bcrypt.compare(
+    "secret",
+    "$2a$10$.Bldj9B8illgKyUqZcgjYefErKj5Y0u1MiWSiwE1MKD0L6MA7CY2u",
+    function(err, res) {
+      console.log("first guess", res);
+    }
+  );
+  bcrypt.compare(
+    "notasecret",
+    "$2a$10$.Bldj9B8illgKyUqZcgjYefErKj5Y0u1MiWSiwE1MKD0L6MA7CY2u",
+    function (err, res) {
+      console.log("second guess", res);
+    }
+  );
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+  res.json(database.users[database.users.length - 1])
+})
+
+app.get('/profile/:id', (req, res) => {
+  const {id} = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id){
+      found = true;
+      return res.json(user);
+    }
+  })
+  if (!found) {
+    res.status(400).json('no such user')
+  }
+})
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  })
+  if (!found) {
+    res.status(400).json("not found");
+  }
+})
+
+app.listen(4000, () => {
+  console.log('app is running on port 4000');
+})
+
+// / --> res = this is working
+// /signin --> POST success/fail
+// /register --> POST user
+// /profile/:userId --> GET = user
+// /image --> PUT --> user "count of the images checked"
