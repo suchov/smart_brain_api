21a457addecca72c0ffadca35d72ea8cfc954740
suchov <artem.suchov@gmail.com>
2019-06-06 19:48:04 +0300

    move the register work with the DB'

--- server.js
+++ server.js
@@ -37,7 +37,12 @@ const database = {
   ]
 };
 
-console.log(knex.select("*").from("users"));
+knex
+  .select("*")
+  .from("users")
+  .then(data => {
+    console.log(data);
+  });
 
 app.get("/", (req, res) => {
   res.send(database.users);
@@ -70,28 +75,33 @@ app.post("/register", (req, res) => {
       console.log("second guess", res);
     }
   );
-  database.users.push({
-    id: "125",
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date()
-  });
-  res.json(database.users[database.users.length - 1]);
+  knex("users")
+    .returning("*")
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date()
+    })
+    .then(user => {
+      res.json(user[0]);
+    })
+    .catch(err => res.status(400).json("unable to register"));
 });
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(400).json("no such user");
-  }
+  knex
+    .select("*")
+    .from("users")
+    .where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("not found");
+      }
+    })
+    .catch(err => res.status(400).json("error getting user"));
 });
 
 app.put("/image", (req, res) => {
