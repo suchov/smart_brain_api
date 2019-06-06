514cc415f67756b6ad521e87958d1f292bcc7ebb
suchov <artem.suchov@gmail.com>
2019-06-06 20:05:29 +0300

    make the image endpoint working

--- server.js
+++ server.js
@@ -106,17 +106,14 @@ app.get("/profile/:id", (req, res) => {
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(400).json("not found");
-  }
+  knex("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("someting went wrong"));
 });
 
 app.listen(4000, () => {
